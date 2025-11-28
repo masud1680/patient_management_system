73428a6d6d8a0e5c0d00d57cdc913a8cea49f30c
Md. Masud <Masud>
2025-11-28 20:24:22 +0600

    static file collection

--- PMS/settings.py
+++ PMS/settings.py
@@ -150,6 +150,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
