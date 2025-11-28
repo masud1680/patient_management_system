56113299ff3680274f207e3b0cd473ec351c2d39
Md. Masud <Masud>
2025-11-28 19:23:52 +0600

    update online render pgAdmin

--- PMS/settings.py
+++ PMS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://patient-management-system-rvxp.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['patient-management-system-rvxp.onrender.com', '127.0.0.1']
 
 
 
